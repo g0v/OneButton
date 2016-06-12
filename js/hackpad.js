05df8acbcaf1826ce0375eef7ad102620c9258bb
Lee <jessy1092@gmail.com>
2016-06-13 00:31:43 +0800

    Use cheerio to set hackpad content

--- js/hackpad.js
+++ js/hackpad.js
@@ -1,9 +1,30 @@
 var config = require('../config');
 var Hackpad = require('hackpad');
+var cheerio = require('cheerio');
 
 var client = new Hackpad(HACKPAD.client_id, HACKPAD.secret, { 'site': 'g0v' });
 
 // get template
 client.export('2ucl5ftrzot', 'latest', 'html', function(err, resp) {
-  client.create(resp, 'text/html');
+
+  var $ = cheerio.load(resp, {decodeEntities: false});
+
+  // console.log(resp);
+  // console.log($('body').html());
+
+  // Set hackpad title
+  $('h1').text('hackathxn - 第20次名稱黑客松');
+
+  // Create new pad with title
+  client.create(new Buffer($('h1').html()), 'text/html', function(err, resp) {
+
+    console.log(resp);
+    console.log(err);
+
+    // Copy full content to new pad
+    client.import(resp['padId'], new Buffer($('body').html()), 'text/html', function(err, resp) {
+      console.log(resp);
+    });
+  });
+
 });
