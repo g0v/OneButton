7e90ab4432e906617151ae900c3953553c1dd49f
hlb <hlbyichi@gmail.com>
2016-06-13 00:03:13 +0800

    Finish casper code. It can create event from KKTIX now.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # OneButton
+
+* `brew install phantomjs`
+* `npm install -g casperjs`
+* update config.js
+*  `casperjs --ignore-ssl-errors=yes --ssl-protocol=tlsv1 --slug="EVENT_SLUG" --name="EVENT_NAME" --start_at="2016/06/24 09:00" --end_at="2016/06/24 18:00" js/kktix.js`
--- js/kktix.js
+++ js/kktix.js
@@ -16,8 +16,8 @@ casper.start('https://kktix.com/users/sign_in');
 
 casper.waitForSelector('form', function() {
     this.fill('form[action="/users/sign_in"]', {
-        'user[login]': KKTIX.user,
-        'user[password]': KKTIX.password
+        'user[login]': config.KKTIX.login,
+        'user[password]': config.KKTIX.password
       }, true);
 });
 
@@ -26,15 +26,24 @@ casper.waitForSelector('#nav-user-display-id', function() {
   var user = this.evaluate(function() {
     return document.querySelector('#nav-user-display-id').textContent;
   });
-  this.echo("Your name is " + user);
+  casper.log('User Name: ' + user, 'debug');
 });
 
-/* go to create event and fork from hackathon19 */
-casper.thenOpen('https://kktix.com/dashboard/organizations/g0v-jothon/events/new?fork_from=g0v-hackath19n', function() {
-  var slug = this.evaluate(function(){
-    return document.querySelector('#event_slug').value;
-  });
-  this.echo("slug: " + slug);
+/* fork event from template */
+casper.thenOpen('https://kktix.com/dashboard/organizations/' + config.KKTIX.organization + '/events/new?fork_from=' + config.KKTIX.template, function() {
+  this.fill('form#new-event', {
+    'event[name]': casper.cli.get("name"),
+    'event[slug]': casper.cli.get("slug"),
+    'event[capacity]': 100,
+    'event[start_at]': casper.cli.get("start_at"),
+    'event[end_at]': casper.cli.get("end_at")
+  }, true);
+
+  this.click('.breadcrumb li:nth-child(3)');
+});
+
+casper.waitForSelector('button[type="submit"]', function() {
+  document.querySelector('button[type="submit"]').click();
 });
 
 casper.run();
