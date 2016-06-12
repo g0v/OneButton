b93e88418e2da192f090061e0e06e30ee540f70b
hlb <hlbyichi@gmail.com>
2016-06-13 00:19:34 +0800

    input.js now actually create event for you now

--- js/input.js
+++ js/input.js
@@ -1,32 +1,45 @@
+var config = require('../config');
 var inquirer = require('inquirer');
 var moment = require('moment-natural');
+var exec = require('child_process').exec;
 
 /* next Saturday */
-var default_begin_date = moment.natural('09:00 next saturday').format('YYYY/MM/DD HH:mm');
-var default_end_date = moment.natural('18:00 next saturday').format('YYYY/MM/DD HH:mm');
+var default_start_at = moment.natural('09:00 next saturday').format('YYYY/MM/DD HH:mm');
+var default_end_at = moment.natural('18:00 next saturday').format('YYYY/MM/DD HH:mm');
+
+var isNumeric = function(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+};
 
 var questions = [
   {
     type: 'input',
     name: 'times',
-    message: '第幾次大松'
+    message: '第幾次大松（數字）',
+    validate: function(value) {
+      var pass = isNumeric(value);
+      if (pass) {
+        return true;
+      }
+      return '必須是數字！';
+    }
   },
   {
     type: 'input',
     name: 'name',
     message: '大松名稱',
-    default: function () {
-      return '謝頓危機松';
+    default: function() {
+      return '第廿四次謝頓危機松';
     }
   },
   {
     type: 'input',
-    name: 'begin_at',
+    name: 'start_at',
     message: '開始時間',
-    default: function () {
-      return default_begin_date;
+    default: function() {
+      return default_start_at;
     },
-    validate: function (value) {
+    validate: function(value) {
       var pass = value.match(/(\d{4}\/\d{2}\/\d{2} \d{2}:\d{2})/);
       if (pass) {
         return true;
@@ -38,10 +51,10 @@ var questions = [
     type: 'input',
     name: 'end_at',
     message: '結束時間',
-    default: function () {
-      return default_end_date;
+    default: function() {
+      return default_end_at;
     },
-    validate: function (value) {
+    validate: function(value) {
       var pass = value.match(/(\d{4}\/\d{2}\/\d{2} \d{2}:\d{2})/);
       if (pass) {
         return true;
@@ -52,15 +65,17 @@ var questions = [
 ];
 
 inquirer.prompt(questions).then(function (answers) {
+  var slug = "g0v-hackath" + answers.times + "n";
   console.log('開始...');
   console.log('--------');
   console.log('建立 KKTIX 活動...');
-  console.log('建完活動也發佈啦... URL');
+  var cmd = 'casperjs --ignore-ssl-errors=yes --ssl-protocol=tlsv1 --slug="' + slug + '" --name="' + answers.name + '" --start_at="' + answers.start_at + '" --end_at="' + answers.end_at + '" js/kktix.js';
+  exec(cmd);
+  console.log('建完活動啦... https://kktix.com/dashboard/events/' + slug);
   console.log('--------');
   console.log('建立 hackpad...');
   console.log('建完 hackpad 啦... URL');
   console.log('--------');
   console.log('建立 Google Spreadsheet...');
   console.log('建完 Spreadsheet 啦... URL');
-  console.log(JSON.stringify(answers, null, '  '));
 });
