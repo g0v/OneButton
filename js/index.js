8f7914227fb5d18a14dfc206b0e016d18de23076
hlb <hlbyichi@gmail.com>
2016-06-12 11:52:16 +0800

    init with a button

---
+++ js/index.js
@@ -0,0 +1,6 @@
+$('document').ready(function() {
+	var options = { videoId: 'mSaKTmpzV-0', start: 3 };
+	$('#wrapper').tubular(options);
+	// f-UGhWj1xww cool sepia hd
+	// 49SKbS7Xwf4 beautiful barn sepia
+});
