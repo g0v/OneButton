471c9ebb853cd4234a95e441a867274100cf368a
caasi Huang <caasi.igd@gmail.com>
2016-06-18 13:05:40 +0800

    Fix naive logic bug

--- js/hackfoldr.js
+++ js/hackfoldr.js
@@ -36,7 +36,7 @@ function run(times, name, begin_at, signup_at, spreadsheet_link) {
   times = times || 0;
   name = name || '第零次動員戡亂黑客松';
   begin_at = begin_at || '2012/12/01 09:00';
-  signup_at = signup_at || '2012/12/01 09:00'; // 有人記得第零次的報名時間嗎 XD
+  signup_at = signup_at || '12/01'; // 有人記得第零次的報名時間嗎 XD
   spreadsheet_link = spreadsheet_link || 'http://bit.ly/g0v-hackath' + times + 'n';
 
   var templatePath = path.join(__dirname, '../tmpl/ethercalc.sc');
@@ -64,13 +64,13 @@ function run(times, name, begin_at, signup_at, spreadsheet_link) {
 
 if (runningAsScript) {
   var argv = process.argv;
-  if (argv.length !== 6 || argv.length !== 7) {
-    console.log('./hackfoldr.js <times> <name> <begin_at> <signup_at> [spreadsheet_link]');
+  if (argv.length !== 6 && argv.length !== 7) {
+    // ./hackfoldr.js 321 第三二一次謝頓危機松 2077/10/23+09:00 10/11
+    console.log('./hackfoldr.js <times> <name> <begin_at YYYY/MM/DD+HH:mm> <signup_at MM/DD> [spreadsheet_link]');
     process.exit(-1);
   }
-  // ./hackfoldr.js 321 第三二一次謝頓危機松 2077/10/23+09:00 2077/10/11+12:00
   run(+argv[2], argv[3], argv[4].replace(/\+/g, ' '), argv[5].replace(/\+/g, ' '), argv[6])
-    .then(console.log.bind(console))
+    .then(console.log.bind('https://ethercalc.org' + console))
     .catch(console.error.bind(console));
 } else {
   module.exports = run;
