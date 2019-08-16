063d4f58ced6f7a3ca528eb68b3a025f76725efa
Ronny Wang <ronnywang@gmail.com>
2019-08-16 13:56:10 +0800

    remove shorten url because hackfoldr not supported

--- js/spreadsheet.js
+++ js/spreadsheet.js
@@ -57,15 +57,7 @@ function copyFile(auth) {
         httpClient.parseJSON()
       )
 
-      var promoise = createShorten('/shorten?access_token=' + config.BITLY.access_token + '&longUrl=' + spreadsheet_url)
-        .then(function(resp){
-          var ret = resp.jsonData;
-          if (ret.status_code != 200) {
-              console.log(ret.status_txt);
-          } else {
-              console.log(ret.data.url);
-          }
-      });
+      console.log(spreadsheet_url);
     }
   });
 }
