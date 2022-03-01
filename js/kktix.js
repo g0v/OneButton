cafa07a1eb701231dc3eb6f2d2d1794a26a8c014
王向榮 <wangxiangrong@wangxiangrongdeMacBook-Air.local>
2022-03-01 15:53:35 +0800

    change casperjs to puppeteer

--- js/kktix.js
+++ js/kktix.js
@@ -1,65 +1,77 @@
-var debug = false;
-if (debug) {
-  options = {
-    verbose: true,
-    logLevel: "debug"
-  };
-} else {
-  options = {};
+var params = {};
+for (var argv of process.argv) {
+  var matches = argv.match(/^--([^=]*)=(.*)$/);
+  if (matches) {
+    params[matches[1]] = matches[2];
+  }
 }
-options.waitTimeout = 20000;
 
 var config = require('../config');
-var casper = require('casper').create(options);
+const puppeteer = require('puppeteer');
 
 /* login */
-casper.start('https://kktix.com/users/sign_in');
-
-casper.waitForSelector('form', function() {
-    this.fill('form[action="/users/sign_in"]', {
-        'user[login]': config.KKTIX.login,
-        'user[password]': config.KKTIX.password
-      }, true);
-});
-
-/* confirm if logged in */
-casper.waitForSelector('#nav-user-display-id', function() {
-  var user = this.evaluate(function() {
-    return document.querySelector('#nav-user-display-id').textContent;
+(async () => {
+  const browser = await puppeteer.launch({
   });
-  casper.log('User Name: ' + user, 'debug');
-});
+  const page = await browser.newPage();
+  page.on('console', (msg) => console.log('PAGE LOG:', msg.text()));
 
-/* fork event from template */
-casper.thenOpen('https://kktix.com/dashboard/organizations/' + config.KKTIX.organization + '/events/new?fork_from=' + config.KKTIX.template, function() {
+  await page.goto('https://kktix.com/users/sign_in');
 
-  var name = casper.cli.get("slug").replace('-', ' ') + ' | 台灣零時政府' + casper.cli.get("name");
+  console.log('login to kktix');
+  await page.waitForSelector('form');
+  await page.type('#user_login', config.KKTIX.login);
+  await page.type('#user_password', config.KKTIX.password);
+  await page.click('[type=submit]');
 
-  this.fill('form#new-event', {
-    'event[name]': name,
-    'event[slug]': casper.cli.get("slug"),
-    'event[start_at]': casper.cli.get("start_at"),
-    'event[end_at]': casper.cli.get("end_at")
-  }, true);
 
-  this.click('.breadcrumb li:nth-child(2)');
+  console.log('login to kktix done');
+  /* confirm if logged in */
+  await page.waitForSelector('#nav-user-display-id');
 
-  var end_date_str = casper.cli.get("end_at");
+  /* fork event from template */
+  await page.goto('https://kktix.com/dashboard/organizations/' + config.KKTIX.organization + '/events/new?fork_from=' + config.KKTIX.template);
+
+  var name = params["slug"].replace('-', ' ') + ' | 台灣零時政府' + params['name'];
+
+  await page.evaluate( (name) => document.getElementById("event_name").value = name, name);
+  await page.evaluate( (slug) => document.getElementById("event_slug").value = slug, params['slug']);
+  await page.evaluate( (start_at) => document.getElementById("event_start_at").value = start_at, params['start_at']);
+  await page.evaluate( (end_at) => document.getElementById("event_end_at").value = end_at, params['end_at']);
+
+  await page.click('.breadcrumb li:nth-child(2)');
+  var end_date_str = params['end_at'];
   var end_date = end_date_str.split(' ');
 
-  this.fill('form#new-event', {
-    'tickets[][start_at]': casper.cli.get("signup_at") + ' 12:00',
-    'tickets[][end_at]': end_date[0] + ' 9:00'
-  }, true);
-  
+  await page.evaluate( (signup_at) => {
+    nodes = document.querySelectorAll('[name="tickets[][start_at]"]');
+    for (var i = 0; i < nodes.length; i ++) {
+      node = nodes[i];
+      node.value = signup_at;
+    }
+  }, params['signup_at'] +  ' 12:00');
 
-  this.click('.breadcrumb li:nth-child(3)');
-  
-});
+  await page.evaluate( (end_at) => {
+    nodes = document.querySelectorAll('[name="tickets[][end_at]"]');
+    for (var i = 0; i < nodes.length; i ++) {
+      node = nodes[i];
+      node.value = end_at;
+    }
+  }, end_date[0] + ' 09:00');
 
-casper.waitForSelector('button[type="submit"]', function() {
-  document.querySelector('button[type="submit"]').click();
-});
+  await page.waitForSelector('.breadcrumb li:nth-child(3)');
+  await page.click('.breadcrumb li:nth-child(3)');
 
+  await page.waitForSelector('[name="formSettings[type]"]:nth-child(1)');
+  await page.click('[name="formSettings[type]"]:nth-child(1)');
 
-casper.run();
+  await page.waitForSelector('[name="hasOrderDataDeadline"]:nth-child(1)');
+  await page.click('[name="hasOrderDataDeadline"]:nth-child(1)');
+
+  await page.screenshot({ path: 'all.png' });
+  await page.waitForSelector('button[type="submit"]');
+  await page.click('button[type="submit"]');
+  await page.screenshot({ path: 'all.png' });
+
+  await browser.close();
+})();
